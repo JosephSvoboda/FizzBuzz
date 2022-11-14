6c189e687c9813ffc6fb74d156af68230036efcb
Joseph Svoboda <joseph.svoboda15@yahoo.com>
2022-11-14 15:03:56 -0500

    made a method for fizzbuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,41 @@
+﻿bool o;
+do
+{
+    double userInput;
+    o = double.TryParse(Console.ReadLine(), out userInput);
+    if (o)
+    {
+        void FizzBuzz(double i)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                var a = "Fizzbuzz";
+                Console.WriteLine(a);
+            }
+            else if (i % 5 == 0)
+            {
+                var b = "Buzz";
+                Console.WriteLine(b);
+            }
+            else if (i % 3 == 0)
+            {
+                var c = "Fizz";
+                Console.WriteLine(c);
+            }
+            else
+            {
+                Console.WriteLine("try again");
+            }
+        }
+
+
+
+        FizzBuzz(userInput);
+        
+    }
+
+    if (!o)
+    {
+        Console.WriteLine("try again");
+    }
+} while (!o);
